adb24478ac25774a0f2e9b93da74a38dadba47cc
Kyle Coffey <mariorules24@gmail.com>
2020-06-21 18:23:42 -0500

    Add SDL_Window wrapper

---
+++ src/sdl_wrapper/video/window.cc
@@ -0,0 +1,34 @@
+#include "window.hh"
+#include <sdl_wrapper/sdl_exception.hh>
+
+namespace sdl::video
+{
+Window::Window(video::Context &context, std::string_view title, int x, int y, int w, int h, Uint32 flags)
+{
+    handle = SDL_CreateWindow(title.data(), x, y, w, h, flags);
+    if (handle == nullptr)
+    {
+        throw SDLException("creating window");
+    }
+}
+
+Window::Window(SDL_Window *handle) : handle(handle)
+{
+}
+
+Window::Window(Window &&other)
+{
+    handle = other.handle;
+    other.handle = nullptr;
+}
+
+Window &Window::operator=(Window &&other)
+{
+    if (&other != this)
+    {
+        handle = other.handle;
+        other.handle = nullptr;
+    }
+    return *this;
+}
+} // namespace sdl::video
---
+++ src/sdl_wrapper/video/window.hh
@@ -0,0 +1,39 @@
+/**
+ * @file window.hh
+ * @author Kyle Coffey (kylecoffey1999@gmail.com)
+ * @brief Contains a memory-safe wrapper for the SDL_Window* datatype.
+ * @date 2020-06-21
+ */
+
+#ifndef SDL_WRAPPER_VIDEO_WINDOW_HH
+#define SDL_WRAPPER_VIDEO_WINDOW_HH
+
+#include <SDL2/SDL_video.h>
+#include <string_view>
+
+namespace sdl::video
+{
+
+class Context;
+class Window
+{
+  public:
+    Window(video::Context &context, std::string_view title, int x, int y, int w, int h, Uint32 flags);
+    Window(SDL_Window *handle) __attribute__((nonnull(2)));
+
+    ~Window();
+
+    /// no copy constructor
+    Window(const Window &) = delete;
+    /// no copy assignment
+    Window &operator=(const Window &) = delete;
+
+    Window(Window &&other);
+    Window &operator=(Window &&other);
+
+  private:
+    SDL_Window *handle;
+};
+} // namespace sdl::video
+
+#endif // SDL_WRAPPER_VIDEO_WINDOW_HH
