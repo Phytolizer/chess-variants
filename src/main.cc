98a3e5bd6da9634520b3f74d8ee4a5545355cb88
Kyle Coffey <mariorules24@gmail.com>
2020-06-21 14:36:06 -0500

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,13 @@
+/// @file main.cc
+/// @author Kyle Coffey
+
+/**
+ * @brief The main function.
+ *
+ * @return int The exit status code. 
+ * 0 for success, non-zero for failure.
+ */
+int main()
+{
+    return 0;
+}
