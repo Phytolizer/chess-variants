8d7154056136c49f0f810f8fd22457c633a25f13
Kyle Coffey <mariorules24@gmail.com>
2020-06-21 19:43:36 -0500

    Add event loop to main()

--- src/main.cc
+++ src/main.cc
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sdl_wrapper/context.hh>
 #include <sdl_wrapper/video/window.hh>
+#include <SDL2/SDL_events.h>
 
 using std::cerr;
 
@@ -19,6 +20,20 @@ int main()
     auto videoContext = sdlContext.initVideo();
 
     auto window = videoContext.createWindow("test window", 0, 0, 100, 100).positionCentered().build();
+
+    bool run = true;
+    while (run)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                run = false;
+                break;
+            }
+        }       
+    }
 
     return 0;
 }
