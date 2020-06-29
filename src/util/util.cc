58ea6369bd89d003d84ae47e879e7fe5cfa42c0a
Kyle Coffey <kylecoffey1999@gmail.com>
2020-06-29 13:11:57 -0500

    Explicitly cast std::isspace to bool

--- src/util/util.cc
+++ src/util/util.cc
@@ -5,8 +5,8 @@ namespace util
 std::string trim(std::string_view s)
 {
     std::string s2 = s.data();
-    auto first = std::find_if(s2.begin(), s2.end(), [](char c) { return !std::isspace(c); });
-    auto last = std::find_if(s2.rbegin(), s2.rend(), [](char c) { return !std::isspace(c); });
+    auto first = std::find_if(s2.begin(), s2.end(), [](char c) { return !static_cast<bool>(std::isspace(c)); });
+    auto last = std::find_if(s2.rbegin(), s2.rend(), [](char c) { return !static_cast<bool>(std::isspace(c)); });
     s2.erase(s2.begin(), first);
     s2.erase(last.base(), s2.end());
     return s2;
